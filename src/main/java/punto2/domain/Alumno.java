7b8dcaeebae9185db51e13c6cb287650e2e5f016
FrancoGroselle <fgroselle@frba.utn.edu.ar>
2024-04-03 19:12:01 -0300

    Agrego metodos principales de las clases alumno y materia

--- src/main/java/punto2/domain/Alumno.java
+++ src/main/java/punto2/domain/Alumno.java
@@ -1,4 +1,28 @@
 package punto2.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
 public class Alumno {
+    private String nombre;
+    private String apellido;
+    private String legajo;
+    private List<Materia> aprobadas;
+
+    public Alumno(String nombre, String apellido, String legajo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.legajo = legajo;
+        this.aprobadas = new ArrayList<>();
+    }
+
+    public void agregarMateriasAprobadas(Materia ... materias) {
+        Collections.addAll(this.aprobadas, materias);
+    }
+
+    public boolean tieneCorrelativasAprobadas(Materia materia) {
+        return this.aprobadas.containsAll(materia.getCorrelativas());
+    }
 }
--- src/main/java/punto2/domain/Materia.java
+++ src/main/java/punto2/domain/Materia.java
@@ -1,4 +1,49 @@
 package punto2.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
 public class Materia {
+    private String nombre;
+    private List<Materia> correlativas;
+
+    public Materia(String nombre) {
+        this.nombre = nombre;
+        this.correlativas = new ArrayList<>();
+    }
+
+    public void agregarCorrelativas(Materia ... correlativas) {
+        Collections.addAll(this.correlativas, correlativas);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(correlativas, nombre);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Materia other = (Materia) obj;
+        return Objects.equals(correlativas, other.correlativas) && Objects.equals(nombre, other.nombre);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Materia> getCorrelativas() {
+        return correlativas;
+    }
 }

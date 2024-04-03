b0a17c039cb1730eb32d988d490e696ceb478a41
FrancoGroselle <fgroselle@frba.utn.edu.ar>
2024-04-03 19:12:40 -0300

    Agrego metodos principales de la clase inscripcion

--- src/main/java/punto2/domain/Inscripcion.java
+++ src/main/java/punto2/domain/Inscripcion.java
@@ -1,4 +1,31 @@
 package punto2.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Inscripcion {
+    private static int contador = 0;
+    private Integer id;
+    private Alumno alumno;
+    private List<Materia> materiasaInscribir;
+
+    public Inscripcion(Alumno alumno) {
+        this.id = ++contador;
+        this.alumno = alumno;
+        this.materiasaInscribir = new ArrayList<>();
+    }
+
+    public void agregarMateriasaInscribir(Materia ... materias) {
+        Collections.addAll(this.materiasaInscribir, materias);
+    }
+
+    public boolean aprobada() {
+        for(Materia materia : materiasaInscribir) {
+            if(!this.alumno.tieneCorrelativasAprobadas(materia)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
